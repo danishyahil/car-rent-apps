a81a4b09060658a8dbcd1cd76d7bc5f52fbc6924
dannyyahil <116016519+dannyyahil@users.noreply.github.com>
2023-06-08 00:26:26 +0800

    Add files via upload

---
+++ car-rent-apps-main/post.mode.ts
@@ -0,0 +1,15 @@
+export interface Post {
+    name: string;
+    email: string;
+    enquiry: string;
+    message: string;
+}
+
+export interface Post {
+    car: string;
+    name: string;
+    email: string;
+    contact: string;
+    date: string;
+    
+}
